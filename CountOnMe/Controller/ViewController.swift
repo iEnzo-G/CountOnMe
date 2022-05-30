7889b26aa47d53148c76672185ccab514fcec2aa
Enzo Gammino <96486074+iEnzo-G@users.noreply.github.com>
2022-05-30 13:33:24 +0200

    Update

--- CountOnMe/Controller/ViewController.swift
+++ CountOnMe/Controller/ViewController.swift
@@ -12,123 +12,104 @@ class ViewController: UIViewController {
     
     // MARK: - Outlets
     
-    @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var calculationTextView: UITextView!
     @IBOutlet var numberButtons: [UIButton]!
     @IBOutlet var operatorButtons: [UIButton]!
     
     // MARK: - Properties
     
-    var elements: [String] {
-        return textView.text.split(separator: " ").map { "\($0)" }
-    }
-    
-    // Error check computed variables
-    var expressionIsCorrect: Bool {
-        return elements.last != "+" && elements.last != "-" && elements.last != "÷" && elements.last != "x"
-    }
-    
-    var expressionHaveEnoughElement: Bool {
-        return elements.count >= 3
-    }
-    
-    var canAddOperator: Bool {
-        return elements.last != "+" && elements.last != "-" && elements.last != "÷" && elements.last != "x"
-    }
-    
-    var expressionHaveResult: Bool {
-        return textView.text.firstIndex(of: "=") != nil
-    }
-    
-    
+    let arithmetic: Arithmetic = Arithmetic()
     
     // MARK: - View life cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        allClear()
     }
     
     // MARK: - Actions
     
+    private func allClear() {
+        calculationTextView.text = "0"
+        arithmetic.calculationDisplayArea = "0"
+        operatorButtonsDefaultColor()
+    }
+    
     @IBAction func tappedAllClearButton(_ sender: UIButton) {
-        textView.text = "0"
+        allClear()
     }
     
     @IBAction func tappedNumberButton(_ sender: UIButton) {
         guard let numberText = sender.title(for: .normal) else {
             return
         }
-        
-        if expressionHaveResult {
-            textView.text = ""
-        }
-        
-        textView.text.append(numberText)
+        arithmetic.addNumberTyped(numberText)
+        calculationTextView.text = arithmetic.calculationDisplayArea
     }
     
     @IBAction func tappedOperatorButton(_ sender: UIButton) {
-        operatorButtons.forEach { $0.isSelected = false }
+        guard arithmetic.canAddOperator else {
+            return operandAlreadySetAlert()
+        }
+        operatorButtonsDefaultColor()
         sender.isSelected = true
-        operatorButtons.forEach { $0.backgroundColor = UIColor(named: "Orange")}
-        operatorButtons.forEach { $0.setTitleColor(.white, for: .normal)}
         sender.backgroundColor = .white
         sender.setTitleColor(UIColor(named: "Orange"), for: .selected)
-        var addOperator = ""
-        switch sender.tag {
-        case 0: addOperator = " + "
-        case 1: addOperator = " - "
-        case 2: addOperator = " ÷ "
-        case 3: addOperator = " x "
-        default: break
-        }
-        if canAddOperator {
-            textView.text.append(addOperator)
-        } else {
-            let alertVC = UIAlertController(title: "Error", message: "Operator already set", preferredStyle: .alert)
-            alertVC.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
-            self.present(alertVC, animated: true, completion: nil)
-        }
+        arithmetic.addOperandTyped(sender)
+        calculationTextView.text = arithmetic.calculationDisplayArea
     }
     
     @IBAction func tappedEqualButton(_ sender: UIButton) {
-        guard expressionIsCorrect else {
-            let alertVC = UIAlertController(title: "Error", message: "Incorrect expression", preferredStyle: .alert)
-            alertVC.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
-            return self.present(alertVC, animated: true, completion: nil)
+        guard arithmetic.expressionIsCorrect else {
+           return incorrectExpressionAlert()
         }
-        
-        guard expressionHaveEnoughElement else {
-            let alertVC = UIAlertController(title: "Error", message: "Incomplete calculation !", preferredStyle: .alert)
-            alertVC.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
-            return self.present(alertVC, animated: true, completion: nil)
+        guard arithmetic.expressionHaveEnoughElement else {
+            return incompleteCalculationAlert()
         }
+        operatorButtonsDefaultColor()
+        calculationTextView.text = arithmetic.displayResult()
+    }
+    
+    @IBAction func tappedPercentButton(_ sender: UIButton) {
+        guard arithmetic.expressionIsCorrect else {
+           return incorrectExpressionAlert()
+        }
+        guard arithmetic.canAddOperator else {
+            return operandAlreadySetAlert()
+        }
+//        calculationTextView.text = arithmetic.displayPercent()
+    }
+    
+    @IBAction func tappedRelativeNumber(_ sender: UIButton) {
+    }
+    
+    // MARK: - AlertController functions
+    
+    func operandAlreadySetAlert() {
+        let alertVC = UIAlertController(title: "Error", message: "Operator already set", preferredStyle: .alert)
+        alertVC.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+        self.present(alertVC, animated: true, completion: nil)
+    }
+    
+    func incorrectExpressionAlert() {
+        let alertVC = UIAlertController(title: "Error", message: "Incorrect expression", preferredStyle: .alert)
+        alertVC.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+        return self.present(alertVC, animated: true, completion: nil)
+    }
+    
+    func incompleteCalculationAlert() {
+        let alertVC = UIAlertController(title: "Error", message: "Incomplete calculation !", preferredStyle: .alert)
+        alertVC.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+        return self.present(alertVC, animated: true, completion: nil)
+    }
+    
+    // MARK: - Other functions
+    
+    private func operatorButtonsDefaultColor() {
         operatorButtons.forEach { $0.isSelected = false }
         operatorButtons.forEach { $0.backgroundColor = UIColor(named: "Orange")}
         operatorButtons.forEach { $0.setTitleColor(.white, for: .normal)}
-        
-        // Create local copy of operations
-        var operationsToReduce = elements
-        
-        // Iterate over operations while an operand still here
-        while operationsToReduce.count > 1 {
-            let left = Int(operationsToReduce[0])!
-            let operand = operationsToReduce[1]
-            let right = Int(operationsToReduce[2])!
-            
-            let result: Int
-            switch operand {
-            case "+": result = left + right
-            case "-": result = left - right
-            case "÷": result = left / right
-            case "x": result = left * right
-            default: fatalError("Unknown operator !")
-            }
-            
-            operationsToReduce = Array(operationsToReduce.dropFirst(3))
-            operationsToReduce.insert("\(result)", at: 0)
-        }
-        
-        textView.text = "\(operationsToReduce.first!)"
     }
-
+    
 }
 
---
+++ CountOnMe/Model/Arithmetic.swift
@@ -0,0 +1,111 @@
+//
+//  Arithmetic.swift
+//  CountOnMe
+//
+//  Created by Enzo Gammino on 26/05/2022.
+//  Copyright © 2022 Vincent Saluzzo. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Arithmetic {
+    
+    // MARK: - Properties
+    
+    var calculationDisplayArea: String = "0"
+    
+    private var elements: [String] {
+        return calculationDisplayArea.split(separator: " ").map { "\($0)" }
+    }
+    
+    // Error check computed variables
+    var expressionIsCorrect: Bool {
+        return elements.last != "+" && elements.last != "-" && elements.last != "÷" && elements.last != "x"
+    }
+    
+    var expressionHaveEnoughElement: Bool {
+        return elements.count >= 3
+    }
+    
+    var canAddOperator: Bool {
+        return elements.last != "+" && elements.last != "-" && elements.last != "÷" && elements.last != "x"
+    }
+    
+    private var expressionHaveResult: Bool {
+        return calculationDisplayArea.firstIndex(of: "=") != nil
+    }
+    
+    // MARK: - Calculation functions
+    
+    func addNumberTyped(_ number: String) {
+        if expressionHaveResult {
+            calculationDisplayArea = ""
+        }
+        calculationDisplayArea.append(number)
+    }
+    
+    func addOperandTyped(_ operandButton: UIButton) {
+        var operand = ""
+        if canAddOperator {
+            switch operandButton.tag {
+            case 0: operand = " + "
+            case 1: operand = " - "
+            case 2: operand = " x "
+            case 3: operand = " ÷ "
+            default: fatalError("Unknown operator !")
+            }
+            calculationDisplayArea.append(operand)
+        } else {
+            
+        }
+    }
+    
+    func displayResult() -> String {
+        
+        // Create local copy of operations
+        var operationsToReduce = elements
+        
+        // Iterate over operations while an operand still here
+        while operationsToReduce.count > 1 {
+            let left = Int(operationsToReduce[0])!
+            let operand = operationsToReduce[1]
+            let right = Int(operationsToReduce[2])!
+            
+            if operand == "÷" && right == 0 {
+                return "Error"
+            }
+            
+            let result: Int
+            switch operand {
+            case "+": result = left + right
+            case "-": result = left - right
+            case "÷": result = left / right
+            case "x": result = left * right
+            default: fatalError("Unknown operator !")
+            }
+            operationsToReduce = Array(operationsToReduce.dropFirst(3))
+            operationsToReduce.insert("\(result)", at: 0)
+        }
+        calculationDisplayArea = "\(operationsToReduce.first!)"
+        return calculationDisplayArea
+    }
+    
+//    func displayPercent() -> String {
+//        var operationsToPercent = elements
+//        let number = Float(operationsToPercent[0])!
+//
+//        let result = number * 0.01
+//        operationsToPercent = Array(operationsToPercent.dropFirst(3))
+//        operationsToPercent.insert("\(result)", at: 0)
+//        calculationDisplayArea = "\(operationsToPercent.first!)"
+//        return calculationDisplayArea
+//    }
+    
+}
+
+extension Float {
+    var clean: String {
+       return self.truncatingRemainder(dividingBy: 1) == 0 ? String(format: "%.0f", self) : String(self)
+    }
+}
